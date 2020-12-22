f4a293314e9668bd24ef8fd93707e757383fb1e0
内田麻衣 <maikurodei.biseibutu@gmail.com>
2020-12-22 09:18:51 +0900

    画面タッチでプレイヤーを動かせるようにした

---
+++ Christmas_Santa/Assets/Script/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject touchpanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchpanel.GetComponent<Button>().onClick.AddListener (Click_touchpanel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Click_touchpanel(){
+
+        player.transform.position += new Vector3 (0.0f,2.0f,0.0f);
+    }
+}

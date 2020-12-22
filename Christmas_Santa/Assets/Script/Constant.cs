ce0753125bf0d9fed056818d5fa952734faaed01
内田麻衣 <maikurodei.biseibutu@gmail.com>
2020-12-22 10:35:53 +0900

    画面の下にサンタが落ちた時の判定

--- Christmas_Santa/Assets/Script/Constant.cs
+++ Christmas_Santa/Assets/Script/Constant.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public static class GameInfo{
 
+    //プレイヤーのジャンプ力
+    public static readonly float PLAYER_JUMP = 2.0f;
+
+    //プレイヤーの重力
+    public static readonly float GRAVITY = 0.1f;   
 
 
 
